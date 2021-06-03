b0f7b1153ccf5581eaab454e01e72b2a43ab4b35
Eilhwan <eilhwan@gmail.com>
2021-06-03 21:11:16 +0900

    20210603

---
+++ python/Queue/q.py
@@ -0,0 +1,39 @@
+from collections import deque
+import sys; read = sys.stdin.readline
+# push X: 정수 X를 큐에 넣는 연산이다.
+# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 큐에 들어있는 정수의 개수를 출력한다.
+# empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+# front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+def que(q, com):
+    if com[0] == "push":
+        q.append(com[1])
+    elif com[0] == "pop":
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif com[0] == "size":
+        print(len(q))
+    elif com[0] == "empty":
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif com[0] == "front":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif com[0] == "back":
+        if q:
+            print(q[len(q) - 1])
+        else:
+            print(-1)
+
+n = int(read().rstrip())
+q = deque()
+for i in range(n):
+    com = list(read().rstrip().split())
+    que(q, com)
