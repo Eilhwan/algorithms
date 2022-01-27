1c88a9c22c2c72c462cae578be26bc14eaf53633
Eil <eilhwan@gmail.com>
2022-01-28 00:06:36 +0900

    simple baseball game just for fun

--- python/QuantFun/Baseball.py
+++ python/QuantFun/Baseball.py
@@ -5,16 +5,20 @@ print("Game Start!!!")
 while True:
 
     number = input("please insert 3bit number")
-
-    if int(number) and len(number) == 3:
-        if answer == number:
+    if len(number) == 3:
+        if answer == int(number):
             print("You won the game")
+            break
         else:
             ball = 0
             strike = 0
             for a, b in zip(number, str(answer)):
                 if a == b:
                     strike += 1
+                elif a in str(answer):
+                    ball += 1
+
+            print(f"your number is {strike} strike and {ball}ball")
     elif not int(number):
         print("Plaese insert just number!!!")
     else:
