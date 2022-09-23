462ffb62c6751c11d3f83c83df9bffc7cc4c0f76
Eil <eilhwan@gmail.com>
2022-09-23 12:58:21 +0900

    daikstra

---
+++ algorithms/2022.09.23/Dajikstra-1.py
@@ -0,0 +1,34 @@
+
+
+graph = input()
+distance = [int(10e9) for _ in range(len(graph))]
+visited = [False for _ in range(len(graph))]
+
+def findMin():
+    minValue = int(10e9)
+    index = -1
+
+    for i in range(len(distance)):
+        if distance[i] < min_value and not visited[i]:
+            index = i
+            min_value = distance[i]
+    
+    return index
+
+def daikstra(start):
+    visit[start] = True
+    distance[start] = 0
+
+    for v in range(len(graph[start])):
+        distance[v] = min(distance[v], graph[start][v])
+    
+    for i in range(len(distance) - 1)
+        node = findMin()
+        visited[node] = True
+
+        for j in range(len(graph[i])):
+            cost = graph[i][j]
+            distance[v] = min(distance[j], distance[i] + cost)
+            
+
+daikstra(start=0)
