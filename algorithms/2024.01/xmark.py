859b859ad6c567e2ecbb6cb4a09a709d191bf821
Eilhwan <eilhwan@gmail.com>
2024-01-31 21:00:38 +0900

    0131

---
+++ algorithms/2024.01/xmark.py
@@ -0,0 +1,10 @@
+import sys;input=sys.stdin.readline
+
+n = int(input())
+answer = ""
+for _ in range(n):
+    s, t = input().split()
+    i = s.lower().find("x")
+    if i > -1
+        answer += t[i].upper()
+print(answer)
